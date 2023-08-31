31f4b6ec50d327f8aa1ba0e46e8e2b057340bda4
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2023-08-31 23:12:29 +0600

    update sexond

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("welcome to success your life");
+const express = require("express")
+const app = express()
+app.get("/",(req,res)=>{
+  res.send({
+    message:"heda vore kabo heda vore hagbo"
+  })
+})
+app.listen(port,()=>{
+    console.log("the backend hitting the url path",port);
+})
